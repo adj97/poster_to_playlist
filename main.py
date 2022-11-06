c0a467b67bbe68df4a896dec7b5bc0742f300d87
Andrew Dixon <andrewdixon_97@hotmail.co.uk>
2022-11-06 20:25:38 +0000

    Algocode

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    print("Hello world")
+    print("1. Load image")
+    print("2. Extract names")
+    print("3. Create playlist")
+    print("4. Add artist to playlist")
 
 if __name__ == "__main__":
     exit(main())
